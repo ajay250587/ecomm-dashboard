6110c18e88f1e9bbde85e421dad10eec40954544
Ajay Gupta <ajaygupta936@gmail.com>
2022-07-21 11:24:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+     <h3>E-Commerce Dashboard</h3>
+     <Button>Bootstrap</Button>
+    </div>
+  );
+}
+
+export default App;
